aa1a410c7d9e41706646cd082cb858ed8884ff3c
Douglas Pike <dpike@vericoin.info>
2015-07-17 22:56:13 -0400

    triad update

--- projects/triadFinder.cc
+++ projects/triadFinder.cc
@@ -1,7 +1,7 @@
 //*******************************************************************************************************
 //*******************************************************************************************************
 //********************************                       ************************************************
-//********************************   amberAnalyzer 1.0   ************************************************
+//********************************     triadFiner 1.0    ************************************************
 //********************************                       ************************************************
 //*******************************************************************************************************
 //***************   -Stability Selective Protein Evolution in Implicit Solvent-   ***********************
@@ -25,7 +25,7 @@ int main (int argc, char* argv[])
 	//--Running parameters
 	if (argc !=1)
 	{
-		cout << "amberAnalyzer" << endl;
+        cout << "triadFinder" << endl;
 		exit(1);
 	}
 	enum aminoAcid {A,R,N,D,Dh,C,Cx,Q,E,Eh,Hd,He,Hn,Hp,I,L,K,M,F,P,O,S,T,W,Y,V,G,dA,dR,dN,dD,dDh,dC,dCx,dQ,dE,dEh,dHd,dHe,dHn,dHp,dI,dL,dK,dM,dF,dP,dO,dS,dT,dW,dY,dV};
@@ -43,18 +43,20 @@ int main (int argc, char* argv[])
 
 	//--initialize variables for loop
 	UInt triad, chainNum, resNum, restype, name;
-	dblVec SOcoords, EO1coords, EO2coords, HDcoords, HEcoords;
-	double SOHD, SOHE, EO1HD, EO1HE, EO2HD, EO2HE;
-	stringstream convert;
+    dblVec OHcoords, Hcoords, Ocoords;
+    double OHHe, HeO;
 	string inFrame, outFile;
-    ofstream proteinFile("triad6");
-    double dist = 6;
+    ofstream donors("donors");
+    ofstream histidines("histidines");
+    ofstream acceptors("acceptors");
+    ofstream proteinFile("triads5all");
+    double dist = 5;
 	//ofstream residueFile("residue");
 	//delete bundle;
 
 	//--Run multiple independent evolutions
 	proteinFile << "frame time triad interaction" << endl;
-    for (UInt a = 1; a < 26000; a++)
+    for (UInt a = 1; a < 40000; a++)
 	{ 
 		name = a;
 		stringstream convert; 
@@ -70,16 +72,23 @@ int main (int argc, char* argv[])
 		
 		proteinFile << a << " " << (a*.02) << " ";
 
-		//serine
+        //donor
 		for (UInt i = 0; i < chainNum; i++)
 		{
 			resNum = frame->getNumResidues(i);
 			for (UInt j = 0; j < resNum; j++)
 			{
 				restype = frame->getTypeFromResNum(i, j);
-				if (restype == S)
+                if (restype == S || restype == T)
 				{
-					SOcoords = frame->getCoords(i, j, "OG");
+                    if (restype == S)
+                    {
+                        OHcoords = frame->getCoords(i, j, "OG");
+                    }
+                    if (restype == T)
+                    {
+                        OHcoords = frame->getCoords(i, j, "OG1");
+                    }
 
 					//histidine
 					for (UInt k = 0; k < chainNum; k++)
@@ -89,30 +98,38 @@ int main (int argc, char* argv[])
 							restype = frame->getTypeFromResNum(k, l);
 							if (restype == He)
 							{
-								HDcoords = frame->getCoords(k, l, "ND1");
-					 			HEcoords = frame->getCoords(k, l, "NE2");
-								SOHD = CMath::distance(SOcoords, HDcoords);
-								SOHE = CMath::distance(SOcoords, HEcoords);
-								if (SOHD < dist || SOHE < dist)
+                                Hcoords = frame->getCoords(k, l, "CE1");
+                                OHHe = CMath::distance(OHcoords, Hcoords);
+                                if (OHHe < dist)
 								{
-									//glutamate
+                                    //acceptor
 									for (UInt m = 0; m < chainNum; m++)
 									{
 										for (UInt n = 0; n < resNum; n++)
 										{
 											restype = frame->getTypeFromResNum(m, n);
-											if (restype == E)
+                                            if (restype == E || restype == D || n == resNum-1)
 											{
-												EO1coords = frame->getCoords(m, n, "OE1");
-												EO2coords = frame->getCoords(m, n, "OE2");
-												EO1HD = CMath::distance(EO1coords, HDcoords);
-												EO2HD = CMath::distance(EO2coords, HDcoords);
-												EO1HE = CMath::distance(EO1coords, HEcoords);
-												EO2HE = CMath::distance(EO2coords, HEcoords);
-												if (EO1HD < dist || EO2HD < dist || EO1HE < dist || EO2HE < dist)
+                                                if (restype == E)
+                                                {
+                                                    Ocoords = frame->getCoords(m, n, "CD");
+                                                }
+                                                if (restype == D)
+                                                {
+                                                    Ocoords = frame->getCoords(m, n, "CG");
+                                                }
+                                                if (n == resNum-1)
+                                                {
+                                                    Ocoords = frame->getCoords(m, n, "C");
+                                                }
+                                                HeO = CMath::distance(Ocoords, Hcoords);
+                                                if (HeO < dist)
 												{
 													triad++;
-													proteinFile << triad << " " << i+1 << " " << j+1 << " " << k+1 << " " << l+1 << " " << m+1 << " " << n+1 << " ";
+                                                    proteinFile << triad;
+                                                    donors << j+1 << endl;
+                                                    histidines << l+1 << endl;
+                                                    acceptors << n+1 << endl;
 												}
 											}
 										}
@@ -127,10 +144,13 @@ int main (int argc, char* argv[])
 		if (triad == 0)
 		{
 			proteinFile << triad;
+            donors << triad << endl;
+            histidines << triad << endl;
+            acceptors << triad << endl;
 		}
 		if (triad > 0)
 		{
-            outFile = "triad6_" + countstr + ".pdb";
+            outFile = "triad5more_" + countstr + ".pdb";
 			pdbWriter(frame, outFile);
 		}
 		proteinFile << endl;
