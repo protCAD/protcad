86a8ec9783028c50d4b6d06f98814324881807f8
Douglas Pike <dpike@vericoin.info>
2015-11-30 11:50:21 -0500

    parameters for full ldhelix

--- projects/protEvolver.cc
+++ projects/protEvolver.cc
@@ -51,11 +51,11 @@ int main (int argc, char* argv[])
     srand (getpid());
 
 	//--inputs for mutation
-    UInt activeChains[] = {1};
-    UInt allowedLResidues[] = {A,R,Q,D,E,I,L,K,M,W,V};
-    UInt activeResidues[] = {1,2,3,4,5,6};
-    UInt randomResidues[] = {1,2,3,4,5,6};
-    UInt allowedDResidues[] = {dA,dR,dQ,dD,dE,dI,dL,dK,dM,dW,dV};
+    UInt activeChains[] = {0};
+    UInt allowedLResidues[] = {A,R,Q,E,I,L,K,M,P,W,V};
+    UInt activeResidues[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39};
+    UInt randomResidues[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39};
+    UInt allowedDResidues[] = {dA,dR,dQ,dE,dI,dL,dK,dM,dP,dW,dV};
 
     double phi, bestEnergy, pastEnergy, Energy;
     UInt nobetter = 0, activeChainsSize = sizeof(activeChains)/sizeof(activeChains[0]), randomResiduesSize = sizeof(randomResidues)/sizeof(randomResidues[0]), activeResiduesSize = sizeof(activeResidues)/sizeof(activeResidues[0]);
