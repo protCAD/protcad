50333e49f94427622cc2b29c1a63960d030e693e
effectsToCause <dpike@vericoin.info>
2017-03-01 16:42:19 -0500

    removed incorrect PRO and HYP rotamer files

--- projects/protEnergy.cc
+++ projects/protEnergy.cc
@@ -15,20 +15,20 @@
 //--Program setup-------------------------------------------------------------
 int main (int argc, char* argv[])
 {	
-    string infile = argv[1];
-    PDBInterface* thePDB = new PDBInterface(infile);
-    ensemble* theEnsemble = thePDB->getEnsemblePointer();
-    molecule* pMol = theEnsemble->getMoleculePointer(0);
-    protein* bundle = static_cast<protein*>(pMol);
+	string infile = argv[1];
+	PDBInterface* thePDB = new PDBInterface(infile);
+	ensemble* theEnsemble = thePDB->getEnsemblePointer();
+	molecule* pMol = theEnsemble->getMoleculePointer(0);
+	protein* bundle = static_cast<protein*>(pMol);
 	residue::setCutoffDistance(8.0);
-    residue::setTemperature(300);
-    residue::setElectroSolvationScaleFactor(1.0);
+	residue::setTemperature(300);
+	residue::setElectroSolvationScaleFactor(1.0);
 	residue::setHydroSolvationScaleFactor(1.0);
 	amberElec::setScaleFactor(1.0);
 	amberVDW::setScaleFactor(1.0);
 
 	double Energy = bundle->protEnergy();
-	cout << Energy << " " << infile;
-    pdbWriter(bundle, infile);
+	cout << Energy << " " << infile << endl;
+	pdbWriter(bundle, infile);
 	return 0;
 }
