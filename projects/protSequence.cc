ca98a1b4f25653663afe31be0cea42448cf7b1d6
Douglas Pike <dpike@vericoin.info>
2020-12-18 16:41:30 -0500

    protSequence: update for complete amino acid library and lowercase nomenclature for backbone sequence

--- projects/protSequence.cc
+++ projects/protSequence.cc
@@ -1,7 +1,7 @@
 //*******************************************************************************************************
 //*******************************************************************************************************
 //*************************************                      ********************************************
-//*************************************      protSequence     ********************************************
+//*************************************      protSequence     *******************************************
 //*************************************                      ********************************************
 //*******************************************************************************************************
 //********************************* -get sequence from pdb file- ****************************************
@@ -21,8 +21,8 @@ int main (int argc, char* argv[])
 		exit(1);
 	}
 	enum aminoAcid {A,R,N,D,Dh,C,Cx,Cf,Q,E,Eh,Hd,He,Hp,I,L,K,M,F,P,O,S,T,W,Y,V,G,dA,dR,dN,dD,dDh,dC,dCx,dCf,dQ,dE,dEh,dHd,dHe,dHp,dI,dL,dK,dM,dF,dP,dO,dS,dT,dW,dY,dV,Sf4,Saf,Hem};
-	string aminoAcidString[] = {"A","R","N","D","D","C","C","C","Q","E","E","H","H","H","I","L","K","M","F","P","O","S","T","W","Y","V","G","dA","dR","dN","dD","dDh","dC","dCx","dCf","dQ","dE","dEh","dHd","dHe","dHp","dI","dL","dK","dM","dF","dP","dO","dS","dT","dW","dY","dV","Csf","Sf4","Saf","Hem"};
-	string backboneSeq[] =   { "G", "C", "L", "P", "T","E","Y","A","I","M",  "N",  "D",  "Q",  "R",  "F", "H", "W", "K", "S", "V"};
+	string aminoAcidString[] = {"A","R","N","D","D","C","C","C","Q","E","E","H","H","H","I","L","K","M","F","P","O","S","T","W","Y","V","G","A","R","N","D","D","C","C","C","Q","E","E","H","H","H","I","L","K","M","F","P","O","S","T","W","Y","V","A","R","N","D","D","C","C","C","Q","E","E","H","H","H","I","L","K","M","F","P","O","S","T","W","Y","V","G","A","R","N","D","D","C","C","C","Q","E","E","H","H","H","I","L","K","M","F","P","O","S","T","W","Y","V","A","R","N","D","D","C","C","C","Q","E","E","H","H","H","I","L","K","M","F","P","O","S","T","W","Y","V","G","A","R","N","D","D","C","C","C","Q","E","E","H","H","H","I","L","K","M","F","P","O","S","T","W","Y","V"};
+	string backboneSeq[] =   { "m", "c", "l", "p", "b","t","y","a","i","g",  "n",  "d",  "q",  "r",  "f", "h", "w", "k", "s", "v"};
 	string backboneTypes[] = {"-γ","-π","-α","-ρ","-β","β","ρ","α","π","γ","-γi","-πi","-αi","-ρi","-βi","βi","ρi","αi","πi","γi"};
 	string infile = argv[1];
 	PDBInterface* thePDB = new PDBInterface(infile);
@@ -30,31 +30,28 @@ int main (int argc, char* argv[])
 	molecule* pMol = theEnsemble->getMoleculePointer(0);
 	protein* _prot = static_cast<protein*>(pMol);
 	
-	fstream aa;
+	fstream aa, bb;
 	aa.open ("aa.faa", fstream::in | fstream::out | fstream::app);
-	aa << endl << ">" << infile << " aaseq:" << endl;
-	UInt numRes = _prot->getNumResidues(0);
-	for (UInt j = 0; j < numRes; j++)
-	{
-		UInt restype = _prot->getTypeFromResNum(0,j);
-		aa << aminoAcidString[restype];
-	}
-	aa << endl;
-	aa.close();
-
-	fstream bb;
 	bb.open ("bb.faa", fstream::in | fstream::out | fstream::app);
-	bb << endl << ">" << infile << " bbseq:" << endl;
-	numRes = _prot->getNumResidues(0);
-	for (UInt j = 0; j < numRes; j++)
+	UInt numChains = _prot->getNumChains();
+	for (UInt i = 0; i < numChains; i++)
 	{
-		UInt backboneType = _prot->getBackboneSequenceType(0,j);
-		bb << backboneSeq[backboneType];
+		aa << ">" << infile << "_" << _prot->getChainID(i) << endl;
+		bb << ">" << infile << "_" << _prot->getChainID(i) << endl;
+		UInt numRes = _prot->getNumResidues(i);
+		for (UInt j = 0; j < numRes; j++)
+		{
+			if (!_prot->isCofactor(i,j)){
+				UInt restype = _prot->getTypeFromResNum(i,j);
+				aa << aminoAcidString[restype];
+				UInt backboneType = _prot->getBackboneSequenceType(i,j);
+				bb << backboneSeq[backboneType];
+			}
+		}
+		aa << endl;
+		bb << endl;
 	}
-	bb << endl;
+	aa.close();
 	bb.close();
 	return 0;
 }
-
-
-
