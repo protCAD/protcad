8a1fc979863be1532623f4ead07ac2dea69f933b
Douglas Pike <dpike@vericoin.info>
2021-07-15 15:49:18 -0400

    protEvolver and protMin updates

--- projects/protMin.cc
+++ projects/protMin.cc
@@ -16,18 +16,21 @@
 #include <unistd.h>
 int main (int argc, char* argv[])
 {
-	if (argc !=5)
-	{   cout << "protMin <clearclashes(t/f)> <backboneRelax(t/f)> <inFile.pdb> <outFile.pdb>" << endl;
+	if (argc !=6)
+	{   cout << "protMin <clearclashes(t/f)> <minimize(t/f)> <minbackboneRelax(t/f)> <inFile.pdb> <outFile.pdb>" << endl;
 		exit(1); }
 
 	string clearclashes = argv[1];
-	string bbrelax = argv[2];
-	string infile = argv[3];
-	string outFile = argv[4];
+	string minimize = argv[2];
+	string bbrelax = argv[3];
+	string infile = argv[4];
+	string outFile = argv[5];
 	PDBInterface* thePDB = new PDBInterface(infile);
 	ensemble* theEnsemble = thePDB->getEnsemblePointer();
 	molecule* pMol = theEnsemble->getMoleculePointer(0);
 	protein* _prot = static_cast<protein*>(pMol);
+	bool min = true;
+	if (minimize == "f"){min = false;}
 	bool backbone = false;
 	if (bbrelax == "t"){backbone = true;}
 	bool clash = false;
@@ -41,7 +44,6 @@ int main (int argc, char* argv[])
 	amberElec::setScaleFactor(1.0);
 	amberVDW::setScaleFactor(1.0);
 	residue::setTemperature(300);
-
 	
 	if (clash){
 		UInt startclashes = _prot->getNumHardClashes();
@@ -50,16 +52,18 @@ int main (int argc, char* argv[])
 		UInt endclashes = _prot->getNumHardClashes();
 		cout << "Clashes cleared: " << (int)startclashes-(int)endclashes << endl;
 	}
-	double startE = _prot->protEnergy();
-	cout << "Starting Energy: " << startE << " kcal/mol" << endl;
-	start = clock();
-	_prot->protMin(backbone);
-	end = clock();
-	double endE = _prot->protEnergy();
-	cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-	cout << "Ending Energy: "  << endE << " kcal/mol" << endl;
-	cout << "delta Energy: " << endE-startE << " kcal/mol" << endl;
-	cout << "time: " << cpu_time_used << endl;
+	if (min){
+		double startE = _prot->protEnergy();
+		cout << "Starting Energy: " << startE << " kcal/mol" << endl;
+		start = clock();
+		_prot->protMin(backbone);
+		end = clock();
+		double endE = _prot->protEnergy();
+		cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+		cout << "Ending Energy: "  << endE << " kcal/mol" << endl;
+		cout << "delta Energy: " << endE-startE << " kcal/mol" << endl;
+		cout << "time: " << cpu_time_used << endl;
+	}
 	pdbWriter(_prot, outFile);
 
 	return 0;
